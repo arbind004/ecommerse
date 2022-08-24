3ee81371687f8eb3adcdb123085a2f7987abd75a
Arbind Kumar <arbind004@gmail.com>
2022-08-24 18:57:43 +0530

    first

---
+++ api/index.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+//var parser = require('json-parser');
+const { v4: uuidv4 } = require('uuid');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+app.use(bodyParser.json())
+var dbUrl =  "mongodb+srv://ak004:shTGSHJYS2544@cluster0.y3jbuqv.mongodb.net/";
+var dbName= "ecommerse";
+
+// Use connect method to connect to the server
+MongoClient.connect(dbUrl, function(err, client) {
+    async function run() {
+  
+      try {
+  
+          await client.connect();
+  
+          console.log("MongoDB Connected successfully to server");
+  
+      } catch (err) {
+  
+          console.log(err.stack);
+  
+      }
+  
+      finally {
+  
+          await client.close();
+  
+      }
+  
+  }
+     //const assert = require('assert');
+    // console.log('MongoDB Connected successfully to server');
+    const db = client.db(dbName);
+    app.locals.db = db;
+  });
+  
+// app.use('/api', router);
+
+// router.get('/demo', function(req, res){
+//     res.json("DEMO API TEST");
+//     console.log('/demo api hit');
+// });
+// router.get('/', function(req, res){
+//     res.json("DEMO API TEST");
+//     console.log('/demo api hit');
+// });
+
+app.get('/', (req, res) => {
+    res.json({"msg":"Hello World!"});
+})
+
+
+app.get('/checkversion/:version', (req, res) => {
+    var version=req.params.version;
+    var currentversion="3";
+    if(version >= currentversion)
+    {
+        res.json({"version":version, "newversionavaible":false});
+    }
+    else
+    {
+        res.json({"version":version, "newversionavaible":true});
+    }
+    
+})
+
+app.post('/register', (req, res) => {
+    const db = req.app.locals.db;
+    var email = req.body.email;
+    var name = req.body.name;
+    //var newotp="1234";
+    var newotp = Math.floor(Math.random() * 9000) + 1000;
+
+    const collection = db.collection('otp');
+
+    var data2={'name': name, 'email': email, 'otp': ""+newotp+""};
+    
+     collection.insert(data2, {w:1}, function(err, result) {
+       if (err) {
+         res.end("Registration Error1");
+         console.warn(err.message);  // returns error if no matching object found
+       } else {
+        
+       }
+     });
+
+    res.json({"email":email,"name":name,"register":true,"msg":"Register sucessfull"});
+})
+
+app.post('/otpverify', (req, res) => {
+    var email = req.body.email;
+    var otp = req.body.otp;
+    const db = req.app.locals.db;
+    const collection = db.collection('otp');
+    const collection2 = db.collection('user');
+    var uid = uuidv4();
+    collection.find({"email":email, "otp":otp}).limit(1).sort({_id:-1}).toArray(function (err, result) {
+        if (err) {
+          console.log(err);
+        } else if (result.length) {
+           var name = String(result[0]["name"]);
+            var data2={'name': name, 'email': email, 'akey':uid};
+    
+            collection2.insert(data2, {w:1}, function(err, result) {
+              if (err) {
+                res.end("Registration Error1");
+                console.warn(err.message);  // returns error if no matching object found
+              } else {
+               
+              }
+            });
+            res.json({"email":email,"otpverify":true,"msg":"login sucessfull"});
+        } else {
+            res.json({"email":email,"otpverify":false,"msg":"login failed"});
+        }
+
+         });
+
+    
+})
+
+app.get('/productlist', (req, res) => {
+    res.json({"result":true,"msg":"product list"});
+})
+
+app.listen(4044);
+console.log('Running on port 4044');
+      
